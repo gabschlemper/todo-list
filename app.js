78ce3414935a4156d91487f0c0af11ea2e09ea12
Gabriela Schlemper <gabschlemper@gmail.com>
2020-05-14 17:14:12 -0300

    add option to remove item from the list

--- app.js
+++ app.js
@@ -2,20 +2,45 @@
 var todoList = document.querySelector('.todo') 
 var tituloItem = document.querySelector('input')
 
+var listaDeTarefas = [{
+    title: "teste",
+    completed: false, 
+    trash: false
+}];
 
-function adicionarItem(event) {
+function adicionarItem(objeto) {
+    var nome = objeto.title;
+    var item = "<li><i class='far fa-circle'></i>" + nome + "<i job='delete' class='fas fa-trash'></i></li>"; 
+    todoList.insertAdjacentHTML("beforeend", item);          tituloItem.value = '';      
+}
+
+
+function carregarLista() {
+    listaDeTarefas.forEach(function(item) {
+        adicionarItem(item)
+    })
+}
+
+tituloItem.addEventListener("keyup", function(event){
     if (event.keyCode === 13) {
-        var nome = event.target.value;
-        var item = "<li><i class='far fa-circle'></i>" + nome + "<i class='fas fa-trash'></i></li>"; 
-        todoList.insertAdjacentHTML("beforeend", item);  
-        tituloItem.value = '';      
+        var texto = event.target.value;
+        console.log('enter', texto);
+        adicionarItem({
+            title: texto,
+            completed: false,
+            trash: false
+        })
     }
-} 
+})
 
+todoList.addEventListener('click', function(event){
+    var alvo = event.target;
+    var valorDoATributo = alvo.getAttribute('job')
+    console.log('clicou.....', valorDoATributo)
+    if (valorDoATributo === 'delete') {
 
+    todoList.removeChild(alvo.parentNode);    
+    }
+})
 
-tituloItem.addEventListener("keyup", adicionarItem)
-
-
-
-
+carregarLista ()
