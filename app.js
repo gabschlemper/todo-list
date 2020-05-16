7f651b6e9475a0c0b4013117b1531810759b4c21
Gabriela Schlemper <gabschlemper@gmail.com>
2020-05-16 11:25:27 -0300

    check/uncheck items from the list

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var todoList = document.querySelector('.todo')
 var tituloItem = document.querySelector('input')
 
 var listaDeTarefas = [];
+var id = 0;
 
 function adicionarItem(objeto) {
     var nome = objeto.title;
-    var item = "<li><i class='far fa-circle'></i>" + nome + "<i job='delete' class='far fa-times-circle delete'></i></li>"; 
+    var item = `<li><i id='${objeto.id}' job="check" class="${objeto.completed ? 'far fa-check-circle check' : 'far fa-circle check'}"></i>${nome}<i id='${objeto.id}' job="delete" class="far fa-times-circle delete"></i></li>`;
     todoList.insertAdjacentHTML("beforeend", item); 
     tituloItem.value = '';  
 }
@@ -29,6 +30,7 @@ tituloItem.addEventListener("keyup", function(event){
         var texto = event.target.value;
                 
         var meuObjeto = { 
+            id: id,
             title: texto,
             completed: false,
             trash: false
@@ -36,16 +38,39 @@ tituloItem.addEventListener("keyup", function(event){
         adicionarItem(meuObjeto); 
 
         listaDeTarefas.push(meuObjeto);
-        localStorage.setItem('TODO', JSON.stringify(listaDeTarefas))    
+        id = id + 1;
+        salvarArrayNoLocalStorage (listaDeTarefas)
     }
 })
 
 todoList.addEventListener('click', function(event){
     var alvo = event.target;
     var valorDoATributo = alvo.getAttribute('job')
+
     if (valorDoATributo === 'delete') {
         todoList.removeChild(alvo.parentNode);
+        listaDeTarefas.splice(alvo.id, 1)
+        salvarArrayNoLocalStorage (listaDeTarefas)
+    }
+
+    if (valorDoATributo === 'check') {
+        var itemDoArray = listaDeTarefas[alvo.id];
+
+        if (itemDoArray.completed === true) {
+            itemDoArray.completed = false;
+            alvo.classList.remove('fa-check-circle')
+            alvo.classList.add('fa-circle')
+        } else {
+            itemDoArray.completed = true;
+            alvo.classList.add('fa-check-circle')
+            alvo.classList.remove('fa-circle')
+        }
+        salvarArrayNoLocalStorage (listaDeTarefas) 
     }
 })
+
+function salvarArrayNoLocalStorage (arrayTodo) {
+    localStorage.setItem('TODO', JSON.stringify(arrayTodo));    
+}
 
 carregarLista ()
